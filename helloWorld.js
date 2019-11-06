c14566a8e8b7bf3f31da5183bbfa299310b5a07d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-11-06 09:04:06 -0700

    lol

--- helloWorld.js
+++ helloWorld.js
@@ -1,13 +1,9 @@
-const canvas= document.getElementById('glcanvas');
-
-// Vertex shader program
+const canvas = document.getElementById('glcanvas');
 
 const vsSource = `
 attribute vec4 aVertexPosition;
-
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
-
 void main() {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 }
@@ -15,173 +11,169 @@ void main() {
 
 const fsSource = `
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 `;
 
 const loadShader = (gl, type, source)=>{
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-    return null;
-  }
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
 
-  return shader;
+    return shader;
+}
+
+const initShader = (gl, vsSource, fsSource)=>{
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
 }
 
 
+const initBuffers = gl =>{
+    
+    const positionBuffer = gl.createBuffer();
 
-const initShader =(gl,vsSource, fsSource)=>{
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
-  const shaderProgram = gl.createProgram();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  //SE agrego un shader attach
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  
-  //ya esta, el vertex y el program, este se liga, para que el OPGl, para ienviar informacion cargada.
-  gl.linkProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-    return null;
-  }
-  return shaderProgram;
-}
-
-const initBuffers= gl =>{
-      const positionBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-      const positions = [
+    const positions = [
         -1.0,  1.0,
          1.0,  1.0,
         -1.0, -1.0,
          1.0, -1.0,
       ];
-      gl.bufferData(gl.ARRAY_BUFFER, 
-                    new Float32Array(positions),
-                    gl.STATIC_DRAW);
-       return {
-         position: positionBuffer,
-         };                          
+
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.STATIC_DRAW
+    );
+
+    return {
+        position: positionBuffer
+    };
 }
 
 const drawScene = (gl, programInfo, buffers)=>{
-  gl.clearColor(0.0, 0.0, 0.0, 1.0); 
-  gl.clearDepth(1.0); //sirve para borrar todo
-  gl.enable(gl.DEPTH_TEST); // habilita la profundidad, habilita para colocar capas o colcoar un obejeto sobre otro,
-  gl.depthFunc(gl.LEQUAL);   //obscuresimiento de las cosas , lo que estras de un objeto, lo obsucurese, es decir, se sobre pone.
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //limpiar los buffer(Capas)
+    /* render */
+    gl.clearColor(0, 0, 0, 1);
+    gl.clearDepth(1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
 
-  //Camara(generar)
-  const fieldOfView = 45 * Math.PI / 180; //en radiales
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;//altura y ancho del canvas
-  const zNear = 0.1; //serca empieza a ver
-  const zFar = 100; //lejos termina de ver
-  const projectionMatrix = mat4.create();
-  
-  mat4.perspective(
-    projectionMatrix, 
-    fieldOfView, 
-    aspect, 
-    zNear, 
-    zFar
-  );  
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  const modelViewMatrix = mat4.create();
-      mat4.translate(
-        modelViewMatrix,  //matriz destino
-        modelViewMatrix, 
-        [-0.0, 0.0, -6.0]
-      ); //vectro con el que se hara el cambi
+    /*Camera */
 
-  {
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100;
 
-    //make kuad
-    const numComponents = 2;
-    const type = gl.FLOAT;
-    const normalize = false; //si hay una variable menor a un decimal nu legara mas de 1 a -1; sirve para mantener la direccion
-    const stride = 0;  //cuantos bytes tendra el texto
-    const offset = 0; //van del  lado del boffer que se cargara
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position); //agaarre el bindbuffer y lo cambie por los buffer
-    gl.vertexAttribPointer(
-      programInfo.attribLocations.vertexPosition,
-      numComponents,
-      type,
-      normalize,
-      stride,
-      offset);   //se arrasstra el resto de los componentes que se fueron añadiendo en caad uno
-    gl.enableVertexAttribArray(
-      programInfo.attribLocations.vertexPosition);
-                    
-  }
-  //para decirle al WebGl cuandose esta haciendo
-  gl.useProgram(programInfo.program);
+    const projectionMatrix = mat4.create();
 
-  //que use el shader que fue añadido
-  gl.uniformMatrix4fv(
-    programInfo.uniformLocations.projectionMatrix,
-    false,
-    projectionMatrix);
+    mat4.perspective(
+        projectionMatrix,
+        fieldOfView,
+        aspect,
+        zNear,
+        zFar
+    );
 
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
+    const modelViewMatrix = mat4.create();
 
-      {  
+    mat4.translate(
+        modelViewMatrix,
+        modelViewMatrix,
+        [0, 0, -6]
+    );
+
+    {
+        /*make quad*/
+
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix
+    );
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix
+    );
+
+    {
         const offset = 0;
         const vertexCount = 4;
         gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-      }
-
+    }
 }
 
-
-
-
 const main = ()=>{
-    const gl = canvas.getContext("webgl2");
+    const gl = canvas.getContext('webgl2');
+
     if (!gl) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
-      }
-      const shaderProgram =initShader(gl, vsSource, fsSource)
+    }
 
-      //informacion  basica
-      const programInfo ={
+    const shaderProgram = initShader(gl, vsSource, fsSource);
+
+    const programInfo = {
         program : shaderProgram,
-        attribLocations: {
-          vertexPosition: gl.getAttribLocation(shaderProgram,'aVertexPosition'),
+        attribLocations : {
+            vertexPosition : gl.getAttribLocation(shaderProgram, 'aVertexPosition')
         },
-        uniformLocations: {
-          projectionMatrix: gl.getUniformLocation(shaderProgram, 'uPojectionMatrix'),
-          modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+        uniformLocations : {
+            projectionMatrix : gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix : gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
         }
-      };
-      
-      const buffers = initBuffers(gl);
+    };
 
-      drawScene(gl, programInfo, buffers);
 
-      //Esto se tiene que aprender si o si
-      //agrega el color con el que se limpia
-     // gl.clearColor(0, 0, 0, 1);
-      //Este limpia
-      //gl.clear(gl.COLOR_BUFFER_BIT);
+    const buffers = initBuffers(gl);
+
+    drawScene(gl, programInfo, buffers);
 }
 
-
 window.onload = main;
-
-/*Sheider se parece mas al c que al c++. no nos vamos a meter harcaro al respecto, 
-oslo vamos a copiar un sheider la respecto
-idican como se hara el sombreado el colorado de algo
-el paradigma que mas se usa para el sheider es el "lamber"= una iluminacion sombre eel sombreado sobre algo
-una luz, la direccion o el angulo odne se posisona, depende de "lightdirection" y las nomrlaes de un objetof*/
